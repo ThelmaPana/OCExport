c2631d19fe072c2f3b2de07014026737d8e1ad4b
Thelma Panaiotis <thelma@crans.org>
2024-01-05 16:26:06 +0000

    Functions to plot model interpretation.

--- utils.R
+++ utils.R
@@ -13,6 +13,7 @@ library(castr)
 library(fields)
 library(abind)
 library(oce)
+library(vegan)
 
 # Plots
 library(cmocean)
@@ -74,4 +75,68 @@ percent_na <- function(x) {
     res <- sum(is.na(x)) / length(x)
   }
   return(res)
+}
+
+
+## Plot variable importance ----
+#--------------------------------------------------------------------------#
+ggplot_imp <- function(...) {
+  obj <- list(...)
+  metric_name <- attr(obj[[1]], "loss_name")
+  metric_lab <- paste(metric_name,
+                      "after permutations\n(higher indicates more important)")
+
+  full_vip <- bind_rows(obj) %>%
+    filter(variable != "_baseline_")
+
+  perm_vals <- full_vip %>%
+    filter(variable == "_full_model_") %>%
+    group_by(label) %>%
+    summarise(dropout_loss = mean(dropout_loss))
+
+  p <- full_vip %>%
+    filter(variable != "_full_model_") %>%
+    mutate(variable = fct_reorder(variable, dropout_loss)) %>%
+    ggplot(aes(dropout_loss, variable))
+  if(length(obj) > 1) {
+    p <- p +
+      facet_wrap(vars(label)) +
+      geom_vline(data = perm_vals, aes(xintercept = dropout_loss, color = label),
+                 linewidth = 1.4, lty = 2, alpha = 0.7) +
+      geom_boxplot(aes(color = label, fill = label), alpha = 0.2)
+  } else {
+    p <- p +
+      geom_vline(data = perm_vals, aes(xintercept = dropout_loss),
+                 linewidth = 1.4, lty = 2, alpha = 0.7) +
+      geom_boxplot(fill = "#91CBD765", alpha = 0.4)
+
+  }
+  p +
+    theme(legend.position = "none") +
+    labs(x = metric_lab,
+         y = NULL,  fill = NULL,  color = NULL)
+}
+
+
+## Plot partial dependence plot ----
+#--------------------------------------------------------------------------#
+ggplot_pdp <- function(obj, x) {
+
+  p <-
+    as_tibble(obj$agr_profiles) %>%
+    mutate(`_label_` = stringr::str_remove(`_label_`, "^[^_]*_")) %>%
+    ggplot(aes(`_x_`, `_yhat_`)) +
+    geom_line(data = as_tibble(obj$cp_profiles),
+              aes(x = {{ x }}, group = `_ids_`),
+              linewidth = 0.5, alpha = 0.05, color = "gray50")
+
+  num_colors <- n_distinct(obj$agr_profiles$`_label_`)
+
+  if (num_colors > 1) {
+    p <- p + geom_line(aes(color = `_label_`), linewidth = 1.2, alpha = 0.8)
+  } else {
+    p <- p + geom_line(color = "midnightblue", linewidth = 1.2, alpha = 0.8)
+  }
+
+  p
 }
